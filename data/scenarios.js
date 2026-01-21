36125426aca0ec628ac7c22fd02d9e3a728a38a7
Lumi2024 <161085377+Lumi2024@users.noreply.github.com>
2026-01-21 15:08:56 +0100

    Create scenarios.js

---
+++ data/scenarios.js
@@ -0,0 +1,14 @@
+export const scenarios = {
+  optimiste: {
+    label: "Optimiste",
+    values: [1900, 2000, 2100, 2200, 2300]
+  },
+  realiste: {
+    label: "Réaliste",
+    values: [1500, 1550, 1600, 1650, 1700]
+  },
+  prudent: {
+    label: "Prudent",
+    values: [1200, 1250, 1300, 1350, 1400]
+  }
+};
